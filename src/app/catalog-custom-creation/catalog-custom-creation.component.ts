6968c77c35a7e6ccc0cce2342babe7528f26adea
MarcoCarneiro <1160777@isep.ipp.pt>
2019-01-02 23:09:19 +0000

    Added funcionality to add and remove products for the creation of a custom catalog.

--- src/app/catalog-custom-creation/catalog-custom-creation.component.ts
+++ src/app/catalog-custom-creation/catalog-custom-creation.component.ts
@@ -4,6 +4,8 @@ import { Catalog } from '../model/catalog';
 import { Product } from '../model/product';
 import { CatalogService } from '../catalog.service';
 import { ProductService } from '../product.service';
+import { SelectMultipleControlValueAccessor } from '@angular/forms';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-catalog-custom-creation',
@@ -16,22 +18,32 @@ export class CatalogCustomCreationComponent implements OnInit {
   @Input() name: string;
   @Input() description: string;
 
+  size: number;
   products: Product[];
+  current_product: Product;
   selectedProducts: Product[];
+  //selectedProducts: Array<Product>;
 
   constructor(
     private catalogService: CatalogService,
     private productService: ProductService,
     private location: Location
-  ) { }
+  ) {
+    this.catalog = new Catalog();
+    this.selectedProducts = [];
+  }
 
   ngOnInit() {
     this.displayList();
   }
 
+
   displayList(): void {
     this.productService.getProducts()
-      .subscribe(products => this.products = products);
+      .subscribe(products => {
+        this.size = products.length;
+        this.products = products;
+      });
   }
 
   createCustomCatalog(prodList: Product[]): void {
@@ -42,12 +54,36 @@ export class CatalogCustomCreationComponent implements OnInit {
     this.catalogService.addCatalog(this.catalog);
   }
 
-  updateCheckedProducts(option, event) {
-    //updates the selectedProducts array
+  addProductToList(product: Product): void {
+    this.productService.getProduct(product.productId).subscribe(
+      prod => {
+        this.current_product = prod;
+        console.log(prod.name);
+        this.selectedProducts.push(this.current_product);
+      }
+    );
   }
 
-  getSelectedProducts(): any {
-    //retriveve selected products in checkboxes
+  removeProductFromList(product: Product): void {
+    console.log("To delete : ", product.name);
+    if (this.selectedProducts.length == 0) {
+      console.log("Empty array. Exiting");
+      return;
+    } else {
+      for (let prod of this.selectedProducts) {
+        if (prod.productId == product.productId) {
+          const index = this.selectedProducts.indexOf(prod);
+          if (index !== -1) {
+            console.log("Deleting : ", this.selectedProducts[index].name);
+            this.selectedProducts.splice(index, 1);
+          }
+        }
+      }
+    }
+  }
+
+  getSelectedProducts(): Product[] {
+    return this.selectedProducts;
   }
 
   reset(): void {
@@ -55,12 +91,13 @@ export class CatalogCustomCreationComponent implements OnInit {
   }
 
   save(): void {
-    let productFinalList : Product[];
+    let productFinalList: Product[];
     productFinalList = this.getSelectedProducts();
     this.createCustomCatalog(productFinalList);
   }
 
   goBack(): void {
+    //console.log("this.size : ", this.size);
     this.location.back();
   }
 
