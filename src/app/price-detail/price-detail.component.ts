e927c4135c7cf341c8a17fbf9e3743ee22547484
LAPTOP-DM\David <1161110@isep.ipp.pt>
2019-01-03 15:21:21 +0000

    price-detail update

--- src/app/price-detail/price-detail.component.ts
+++ src/app/price-detail/price-detail.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { PriceService } from '../price.service';
+import { Price } from '../model/price';
+
 
 @Component({
   selector: 'app-price-detail',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./price-detail.component.css']
 })
 export class PriceDetailComponent implements OnInit {
+  @Input() price: Price;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private priceService: PriceService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getPrice();
+  }
+
+  getPrice(): void {
+    const id = +this.route.snapshot.paramMap.get('priceId');
+    this.priceService.getPrice(id)
+      .subscribe(price => this.price = price);
+  }
+
+  save(): void {
+    this.priceService.updatePrice(this.price)
+      .subscribe(() => window.location.reload());
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
