a215eaec579a4c0fe25b889dfa5e78b300640964
Hugo Fernandes <1161155@isep.ipp.pt>
2019-01-04 16:19:44 +0000

    Collection service and methods, catalog fix nomenclature

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Location} from '@angular/common';
+import {Collection} from '../model/collection';
+import {CollectionService} from "../collection.service";
+
 
 @Component({
-  selector: 'app-collection',
-  templateUrl: './collection.component.html',
-  styleUrls: ['./collection.component.css']
+    selector: 'app-collection',
+    templateUrl: './collection.component.html',
+    styleUrls: ['./collection.component.css']
 })
 export class CollectionComponent implements OnInit {
 
-  constructor() { }
+    @Input() collection: Collection;
 
-  ngOnInit() {
-  }
+    collections: Collection[];
 
+    constructor(private collectionService: CollectionService, private location: Location) {
+    }
+
+    ngOnInit() {
+        this.displayList();
+    }
+
+    displayList(): void {
+        this.collectionService.getCollections().subscribe(collections => this.collections = collections);
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
+
+    delete(collection: Collection): void {
+        this.collectionService.deleteCollection(collection).subscribe(() => window.location.reload());
+    }
 }
